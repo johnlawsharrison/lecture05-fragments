f7f3d627c812b9a8ca3bd061fb1653d586112f54
John Laws Harrison <johnlawsharrison@gmail.com>
2017-04-11 14:20:01 -0700

    Refactor searchlist into fragment

--- app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
+++ app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
@@ -12,31 +12,18 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import static edu.uw.fragmentdemo.MovieDownloader.downloadMovieData;
+import static edu.uw.fragmentdemo.R.id.listView;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-
-    private ArrayAdapter<Movie> adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        adapter = new ArrayAdapter<Movie>(this,
-                R.layout.list_item, R.id.txtItem, new ArrayList<Movie>());
-
-        ListView listView = (ListView)findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Movie movie = (Movie)parent.getItemAtPosition(position);
-                Log.v(TAG, "You clicked on: "+movie);
-            }
-        });
     }
 
 
@@ -46,33 +33,6 @@ public class MainActivity extends AppCompatActivity {
         String searchTerm = text.getText().toString();
 
         downloadMovieData(searchTerm);
-    }
-
-    //helper method for downloading the data via the MovieDownloadTask
-    public void downloadMovieData(String searchTerm){
-        Log.v(TAG, "You searched for: "+searchTerm);
-        MovieDownloadTask task = new MovieDownloadTask();
-        task.execute(searchTerm);
-    }
-
-    //A task to download movie data from the internet on a background thread
-    public class MovieDownloadTask extends AsyncTask<String, Void, ArrayList<Movie>> {
-
-        @Override
-        protected ArrayList<Movie> doInBackground(String... params) {
-            ArrayList<Movie> data = MovieDownloader.downloadMovieData(params[0]);
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Movie> movies) {
-            super.onPostExecute(movies);
-
-            adapter.clear();
-            for(Movie movie : movies){
-                adapter.add(movie);
-            }
-        }
     }
 
 }
--- app/src/main/java/edu/uw/fragmentdemo/MoviesFragment.java
+++ app/src/main/java/edu/uw/fragmentdemo/MoviesFragment.java
@@ -1,11 +1,18 @@
 package edu.uw.fragmentdemo;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class MoviesFragment extends Fragment {
 
+    private static final String TAG = "MoviesFragment";
+    private ArrayAdapter<Movie> adapter;
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -22,8 +31,52 @@ public class MoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_movies, container, false);
+
+        adapter = new ArrayAdapter<Movie>(getActivity(),
+                R.layout.fragment_movies, R.id.txtItem, new ArrayList<Movie>());
+
+        ListView listView = (ListView)rootView.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Movie movie = (Movie)parent.getItemAtPosition(position);
+                Log.v(TAG, "You clicked on: " + movie);
+            }
+        });
+
+        return rootView;
+
     }
 
+    //helper method for downloading the data via the MovieDownloadTask
+    public void downloadMovieData(String searchTerm){
+        Log.v(TAG, "You searched for: " + searchTerm);
+        MovieDownloadTask task = new MovieDownloadTask();
+        task.execute(searchTerm);
+    }
+
+    //A task to download movie data from the internet on a background thread
+    public class MovieDownloadTask extends AsyncTask<String, Void, ArrayList<Movie>> {
+
+        @Override
+        protected ArrayList<Movie> doInBackground(String... params) {
+            ArrayList<Movie> data = MovieDownloader.downloadMovieData(params[0]);
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Movie> movies) {
+            super.onPostExecute(movies);
+
+            adapter.clear();
+            for(Movie movie : movies){
+                adapter.add(movie);
+            }
+        }
+    }
 }
